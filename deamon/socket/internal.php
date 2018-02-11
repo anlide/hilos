14d04a0b464dcd7f58d06ee719a72be3503efaeb
Alexander Baranov <elfanlide@gmail.com>
2018-02-11 20:40:31 +0300

    Sync composer

---
+++ deamon/socket/internal.php
@@ -0,0 +1,23 @@
+<?php
+namespace Hilos\Deamon\Socket;
+
+class Internal extends Connection {
+  private $failCount = 0;
+
+  /**
+   * Called when new data received.
+   * @return boolean|string
+   */
+  public function onRead() {
+    if ($this->closed) return false;
+    if (($line = $this->readLine(PHP_EOL)) === null) {
+      $this->failCount++;
+      if ($this->failCount > 10) {
+        $this->close(self::CLOSE_ABNORMAL);
+      }
+      return false;
+    }
+    $this->failCount = 0;
+    return $line;
+  }
+}
